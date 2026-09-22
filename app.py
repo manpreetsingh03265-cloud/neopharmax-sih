1c0ad7c0d37c97af0ec346f27d46347260193f68
manpreetsingh03265-cloud <manpreetsingh03265@gmail.com>
2026-09-22 19:06:13 +0530

    Change redirect to login_choice in login_required

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def login_required(f):
     @wraps(f)
     def w(*a,**k):
         if "user_id" not in session:
-            return redirect(url_for("login"))
+           return redirect(url_for("login_choice"))
         return f(*a,**k)
     return w
 
